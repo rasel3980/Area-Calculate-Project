18603af48181a0896987b0da2643386dabb12181
rasel3980 <rkrasel3980@gmail.com>
2026-02-19 15:15:28 +0600

    route handle by server

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/triangle',(req,res)=>{
+    res.sendFile(__dirname + '/triangle.html')
+})
+app.get('/circle',(req,res)=>{
+    res.sendFile(__dirname + '/circle.html')
+})
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`);
 })
